566e266d3b021ecf3b2936103e04c342c761bf5c
alfonsus20 <alfonschandrawan@gmail.com>
2022-02-10 22:29:35 +0700

    fix module styling issue

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 function MyApp({ Component, pageProps, router }) {
   useEffect(() => {
-    router.push(router.pathname);
+    router.push(router.asPath);
   }, []);
 
   return (
