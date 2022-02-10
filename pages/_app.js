1cfa65e06922131218c9ad2c621462d99dc8e740
alfonsus20 <alfonschandrawan@gmail.com>
2022-02-10 23:04:25 +0700

    fix unstyled components on route change

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { UserWrapper } from "../context/userContext";
 import "../styles/globals.scss";
 import { AnimatePresence } from "framer-motion";
 import { useEffect } from "react";
+import Router from "next/router";
 
 function MyApp({ Component, pageProps, router }) {
   useEffect(() => {
@@ -16,5 +17,18 @@ function MyApp({ Component, pageProps, router }) {
     </UserWrapper>
   );
 }
+
+const routeChange = () => {
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange );
+Router.events.on("routeChangeStart", routeChange );
 
 export default MyApp;
