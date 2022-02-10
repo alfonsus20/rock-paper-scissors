062261d9ee4741720b12da137002d94df98e112d
alfonsus20 <alfonschandrawan@gmail.com>
2022-02-10 22:27:37 +0700

    fix module styling issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import { UserWrapper } from "../context/userContext";
 import "../styles/globals.scss";
 import { AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps, router }) {
+  useEffect(() => {
+    router.push(router.pathname);
+  }, []);
+
   return (
     <UserWrapper>
       <AnimatePresence exitBeforeEnter initial={false}>
